248be24ee371b5bd543f62aa705a9f4d6d890ed2
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-03-22 14:07:36 -0400

    Packaged main.py into functions.

--- Backend/main.py
+++ Backend/main.py
@@ -1,19 +1,27 @@
 from calendar_import import ics_to_dict
+import json
+
+def load_ics_file(ics_file_path):
+    # ics_file_path = '../acorn_calendar.ics'
+    try:
+        with open(ics_file_path, 'r') as file:
+            ics_data = file.read()
+        success = True
+    except FileNotFoundError:
+        success = False
+        ics_data = ""
+    return ics_data
 
 # Path to the .ics file
 ics_file_path = '../acorn_calendar.ics'
 
-# Reading the content of the .ics file
-try:
-    with open(ics_file_path, 'r') as file:
-        ics_data_from_file = file.read()
-    success = True
-except FileNotFoundError:
-    success = False
-    ics_data_from_file = ""
+initial_student_schedule = ics_to_dict(load_ics_file(ics_file_path))
 
-print(ics_data_from_file)
+# Find empty time slots within initial_student_schedule
 
-print(ics_to_dict(ics_data_from_file))
+def pretty_print_schedule(schedule):
+    reduced_events_json = json.dumps(schedule, indent=4)
 
+    print(reduced_events_json)
 
+pretty_print_schedule(initial_student_schedule)
