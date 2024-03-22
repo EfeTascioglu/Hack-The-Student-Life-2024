05be297833dbaeb077a36885d7a0b1986ec66ff8
Efe Tascioglu <efe.tascioglu@mail.utoronto.ca>
2024-03-22 15:22:22 -0400

    Started adding GPT integration

--- Backend/application.py
+++ Backend/application.py
@@ -2,8 +2,29 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import base64
 import os
+from openai import OpenAI
 
-DEBUG = True
+def fetch_credentials(dir='C:/Users/Efe/.openAI'):
+    file_name = dir + '/credentials'
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        access_key_id = lines[1].split('=')[1].strip()
+        secret_key = lines[2].split('=')[1].strip()
+        return secret_key
+
+client = OpenAI(api_key=fetch_credentials())
+# Try to use GPT3.5-Turbo, then Baggage and Ada (which are about 4 times cheaper)
+ # PUT YOUR OWN KEY HERE
+
+def request_chat_gpt(prompt):
+    completion = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}]
+    #max_tokens=100,
+    #'temperature=0.9,
+    #top_p=1.0,
+    #frequency_penalty=0.0,
+    #presence_penalty=0.0,
+    #stop=["\n", " Human:", " AI:"]
 
 def fetch_credentials(location="C:/Users/Efe/.AWS/credentials"):
     with open(location, 'r') as file:
