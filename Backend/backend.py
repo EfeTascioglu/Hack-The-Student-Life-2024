53245add20490298384b07408b948ce79985a9e4
Andrew Magnuson <ajwm8103@users.noreply.github.com>
2024-03-22 14:56:34 -0400

    Backend modified slightly

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import base64
 import os
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/api/ping', methods=['GET'])
 def api_ping():
